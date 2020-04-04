ab73f3c15d79e2ffa5617a4652121f24f364a31a
Hudihka <k.iroshnikov@itmegastar.com>
2020-04-05 02:03:05 +0700

    добавленно KVC

--- паралакс хедера/Новый/paralax/paralax/DefaultVC/HederView/HederView.swift
+++ паралакс хедера/Новый/paralax/paralax/DefaultVC/HederView/HederView.swift
@@ -16,6 +16,19 @@ class HederView: UIView{
     @IBOutlet private weak var upConstreint: NSLayoutConstraint!
     
     @IBOutlet weak var imageContent: UIImageView!
+	
+	var SCView: UIScrollView? = nil
+	
+	init(image: UIImage?, superView: UIScrollView, height: CGFloat) {
+		
+		let rect = CGRect(origin: .zero, size: CGSize(width: superView.frame.width, height: height))
+		
+		super.init(frame: rect)
+		
+		self.imageContent.image = image
+		
+		self.SCView = superView
+	}
     
     override init (frame: CGRect) {
         super.init(frame: frame)
@@ -37,47 +50,46 @@ class HederView: UIView{
     }
 
     private func settingsView() {
-        self.downParalaxArray += horizontalConstreints
-        self.downParalaxArray.append(upConstreint)
+		imageContent.contentMode = .scaleAspectFill
+		conteinerView.clipsToBounds = false
+		
+		SCView?.addObserver(self,
+							forKeyPath: #keyPath(UIScrollView.contentOffset),
+							options: [.new, .old],
+							context: nil)
     }
     
     
-    func paralax(scrollView: UIScrollView) {
-        let delta: CGFloat = scrollView.contentOffset.y
-        
-        addSettings(deltaNegative: delta < 0)
-        if delta < 0 {
-            swipeDownParalax(delta: delta)
-        } else if delta > 0{
-            swipeUpParalax(delta: delta / 2)
-        } else { //== 0
-            
-        }
-        
-    }
-    
-    
-    private func swipeDownParalax(delta: CGFloat){
-        
-        for obj in downParalaxArray {
-            obj.constant = delta
-        }
-    }
-    
-    private func swipeUpParalax(delta: CGFloat){
-        
-        upConstreint.constant = delta
-        downConstrein.constant = -1 * delta
+    private func paralax(value: CGFloat) {
+		
+		
+//        let delta: CGFloat = scrollView.contentOffset.y
+//
+//        addSettings(deltaNegative: delta < 0)
+//        if delta < 0 {
+//            swipeDownParalax(delta: delta)
+//        } else if delta > 0{
+//            swipeUpParalax(delta: delta / 2)
+//        } else { //== 0
+//
+//        }
         
     }
-    
-    private func addSettings(deltaNegative: Bool){
-        if deltaNegative, clipsToBounds{
-            clipsToBounds = false
-        } else if !clipsToBounds, !deltaNegative{
-            clipsToBounds = true
+	
+	//MARK - KVC
+	
+	override open func observeValue(forKeyPath keyPath: String?,
+									of object: Any?, change: [NSKeyValueChangeKey : Any]?,
+									context: UnsafeMutableRawPointer?) {
+		
+        if keyPath == #keyPath(UIScrollView.contentOffset), let scroll = self.SCView {
+			
+			let offset = scroll.contentOffset.y
+			
+			print(offset)
         }
     }
+    
 
 
     deinit {
