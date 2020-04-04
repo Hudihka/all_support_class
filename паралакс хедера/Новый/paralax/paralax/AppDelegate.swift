5d4fcb38d0720f9b4a3fcc5114212a8dee4dd077
Hudihka <k.iroshnikov@itmegastar.com>
2020-04-05 00:37:21 +0700

    создание проекта

---
+++ паралакс хедера/Новый/paralax/paralax/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  paralax
+//
+//  Created by Hudihka on 05/04/2020.
+//  Copyright © 2020 Tatyana. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+
+
+}
+
