3bf79a9f4afc54c02616ffc385bc874ebd6e04d1
Hudihka <k.iroshnikov@itmegastar.com>
2020-04-05 01:20:55 +0700

    созданы таблицы

--- паралакс хедера/Новый/paralax/paralax/CustomController.swift
+++ паралакс хедера/Новый/paralax/paralax/CustomController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class CustomController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+	
+	fileprivate var dataArray = [String]()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+		for _ in 0...30{
+			let number = arc4random()
+			dataArray.append("\(number)")
+		}
+		
+		desingTV()
     }
     
 
@@ -26,4 +35,33 @@ class CustomController: UIViewController {
 		return VC as! CustomController
 	}
 
+}
+
+
+extension CustomController: UITableViewDelegate, UITableViewDataSource{
+	
+	fileprivate func desingTV(){
+		self.tableView.delegate = self
+		self.tableView.dataSource = self
+		
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+	}
+	
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return dataArray.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		
+		
+		
+		cell.textLabel?.text = dataArray[indexPath.row]
+		
+		return cell
+	}
+	
+	
 }
--- паралакс хедера/Новый/paralax/paralax/DefaultController.swift
+++ паралакс хедера/Новый/paralax/paralax/DefaultController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class DefaultController: UIViewController {
-
+	
+	@IBOutlet weak var tableView: UITableView!
+	
+	fileprivate var dataArray = [String]()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+		for _ in 0...30{
+			let number = arc4random()
+			dataArray.append("\(number)")
+		}
+		
+		desingTV()
     }
     
 	static func route() -> DefaultController {
@@ -25,4 +34,32 @@ class DefaultController: UIViewController {
 		return VC as! DefaultController
 	}
 
+}
+
+extension DefaultController: UITableViewDelegate, UITableViewDataSource{
+	
+	fileprivate func desingTV(){
+		self.tableView.delegate = self
+		self.tableView.dataSource = self
+		
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+	}
+	
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return dataArray.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		
+		
+		
+		cell.textLabel?.text = dataArray[indexPath.row]
+		
+		return cell
+	}
+	
+	
 }
