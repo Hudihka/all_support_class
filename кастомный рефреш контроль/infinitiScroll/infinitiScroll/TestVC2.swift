9d1a34daa963fd423f7d1879d28cb4ce9c040032
Hudihka <k.iroshnikov@itmegastar.com>
2020-05-02 17:03:00 +0700

    меняем статус бар

--- кастомный рефреш контроль/infinitiScroll/infinitiScroll/TestVC2.swift
+++ кастомный рефреш контроль/infinitiScroll/infinitiScroll/TestVC2.swift
@@ -10,75 +10,50 @@ import UIKit
 
 class TestVC2: UIViewController {
 	
-	//	https://www.youtube.com/watch?v=IWujpBGqxfQ
-	@IBOutlet weak var slider: UISlider!
+	@IBOutlet weak var viewTest: UIView!
 	
-	var cirkleView: CircleView?
-	var cirkleView2: CircleView?
+	var imageStrat = UIImageView()
+	var labelFinish = UILabel()
+	var isFlip = false
 	
-	private var animateCirkles = false
-	
-    override func viewDidLoad() {
+	override func viewDidLoad() {
         super.viewDidLoad()
+		
+		let rect = CGRect(origin: .zero, size: viewTest.frame.size)
 
-				
-		cirkleView = CircleView(frame: CGRect(x: 50, y: 100, width: 60, height: 60))
-		cirkleView?.startValue = EnumAngels.zero.finishValue(from: false)
-		cirkleView?.finishValue = EnumAngels.zero.finishValue(from: true)
-		cirkleView?.clockwise = true
-		self.view.addSubview(cirkleView!)
+		imageStrat = UIImageView(frame: rect)
+		imageStrat.image = UIImage(named: "testImg1")
 		
+		labelFinish = UILabel(frame: rect)
+		labelFinish.text = "TEXT"
+		labelFinish.textColor = .white
+		labelFinish.backgroundColor = .red
+		labelFinish.textAlignment = .center
+		labelFinish.font = UIFont.systemFont(ofSize: 15, weight: .bold)
 		
-		cirkleView2 = CircleView(frame: CGRect(x: 65, y: 115, width: 30, height: 30))
-		cirkleView2?.startValue = EnumAngels.sixHours.finishValue(from: true)
-		cirkleView2?.finishValue = EnumAngels.sixHours.finishValue(from: false)
-		cirkleView2?.clockwise = false
-		self.view.addSubview(cirkleView2!)
-		
-    }
-	
-	
-	@IBAction func actionSlider(_ sender: UISlider) {
-		
-		lenghntCirkle(letnght: CGFloat(sender.value))
-		
+		viewTest.addSubview(labelFinish)
+		viewTest.addSubview(imageStrat)
 	}
 	
 	
 	@IBAction func actionCancel(_ sender: UIButton) {
-		if animateCirkles {
-			stopedAnimateRotate()
+	
+		isFlip = !isFlip
+		let startView = isFlip ? imageStrat : labelFinish
+		let finishView = isFlip ? labelFinish : imageStrat
+		let option: UIView.AnimationOptions = isFlip ? .transitionFlipFromTop : .transitionFlipFromBottom
+		
+		UIView.transition(from: startView,
+						  to: finishView,
+						  duration: 0.5,
+						  options: [.curveEaseOut, option]) { (_) in
+			print("")
 		}
+		
 	}
 	
 	
 	
-	func lenghntCirkle(letnght: CGFloat){
-		if animateCirkles {
-			return
-		}
-		
-		
-		self.cirkleView?.value = letnght
-		self.cirkleView2?.value = letnght
-		
-		if letnght == 1 {
-			animateCirkles = true
-			cirkleView?.infinitiRotate(clockRotate: true, duratiuon: 1, key: "key1")
-			cirkleView2?.infinitiRotate(clockRotate: false, duratiuon: 1, key: "key2")
-			slider.setValue(0, animated: true)
-		}
-	}
-	
-	func stopedAnimateRotate(){
-		cirkleView?.layer.removeAnimation(forKey: "key1")
-		cirkleView2?.layer.removeAnimation(forKey: "key2")
-		
-//		cirkleView?.zeroingCGPath()
-//		cirkleView2?.zeroingCGPath()
-		
-		animateCirkles = false
-	}
 	
 
 }
