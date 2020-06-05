f8ec99719819c002e91b10bd01371bb680b7da85
Константин Худышка <k.iroshnikov@itmegastar.com>
2020-06-05 20:25:10 +0700

    -//-

--- Локальные нотификации/локальные и экстеншен/testLocalPush/testLocalPush/ViewController.swift
+++ Локальные нотификации/локальные и экстеншен/testLocalPush/testLocalPush/ViewController.swift
@@ -1,84 +1,75 @@
 //
 //  ViewController.swift
-//  testLocalPush
+//  UNNotificationContentExtensions
 //
-//  Created by Админ on 05.06.2020.
-//  Copyright © 2020 Админ. All rights reserved.
+//  Created by Erica Millado on 4/30/17.
+//  Copyright © 2017 Erica Millado. All rights reserved.
 //
 
 import UIKit
 import UserNotifications
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var responseLabel: UILabel!
+  
+  @IBOutlet weak var friendImage: UIImageView!
+    
+    
+    
+  @IBAction func triviaBtnTapped(_ sender: Any) {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let friendsContent = UNMutableNotificationContent()
+    friendsContent.title = "Friends Quiz!"
+    friendsContent.subtitle = "Can you identify the Friend with his quote?"
+    friendsContent.body = "'PIVOTTTT!!'"
+    friendsContent.badge = 1
+    friendsContent.categoryIdentifier = "friendsQuizCategory"
+    friendsContent.userInfo = ["category": "friendsQuizCategory"]
+//    friendsContent.sound = UNNotificationSound.default
+    
+    let quizTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
+    
+    let quizRequestIdentifier = "friendsQuiz"
+    let request = UNNotificationRequest(identifier: quizRequestIdentifier, content: friendsContent, trigger: quizTrigger)
+    
+    UNUserNotificationCenter.current().add(request) { (error) in
+      print(error as Any)
     }
     
-    
-    @IBAction func poehali(_ sender: Any) {
-        scheduleNotification()
-    }
-    
-    
-    
-    private func scheduleNotification() {
-        
-        let content = UNMutableNotificationContent()
-        
-        content.title = "EXTENSION"
-        content.subtitle = "Can you identify the Friend with his quote?"
-        content.body = "'PIVOTTTT!!'"
-        content.sound = UNNotificationSound.default
-        content.categoryIdentifier = "UserActionsCategory" //ид категории
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        
-        let identifire = "Local Notification"
-        let request = UNNotificationRequest(identifier: identifire,
-                                            content: content,
-                                            trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request) { (error) in
-            if let error = error {
-                print("Error \(error.localizedDescription)")
-            }
-        }
-        
-        
-    }
-    
-    
-    ///просто локальные
-    
-    @IBAction func local(_ sender: Any) {
-        
-        let content = UNMutableNotificationContent()
-        
-        content.title = "TEST LOCAL"
-        content.body = "Summer Time LOCAL"
-        content.sound = UNNotificationSound.default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        
-        let identifire = "Local Notification LOcal"
-        let request = UNNotificationRequest(identifier: identifire,
-                                            content: content,
-                                            trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request) { (error) in
-            if let error = error {
-                print("Error \(error.localizedDescription)")
-            }
-        }
-        
-        
-    }
-    
-    
-    
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    UNUserNotificationCenter.current().delegate = self
+  }
 
 
 }
+
+//MARK - UNNotification Delegate Methods
+extension ViewController: UNUserNotificationCenterDelegate {
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    
+      completionHandler([.alert])
+  }
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    
+    if response.actionIdentifier == "ross" {
+      responseLabel.text = "That's the correct answer!"
+      friendImage.image = UIImage(named: "test1")
+    } else if response.actionIdentifier == "chandler" {
+      responseLabel.text = "Could you BE more wrong!?"
+      friendImage.image = UIImage(named: "test1")
+    } else {
+      responseLabel.text = "Try again... or go eat a sandwich."
+      friendImage.image = UIImage(named: "test1")
+    }
+    completionHandler()
+  }
+
+}
+
 
