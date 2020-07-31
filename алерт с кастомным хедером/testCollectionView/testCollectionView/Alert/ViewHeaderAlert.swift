d97bb7ec299af51cfc085f0a7b32bebd416c02cb
Константин Худышка <k.iroshnikov@itmegastar.com>
2020-07-31 22:57:08 +0700

    коллекция в алерте

---
+++ алерт с кастомным хедером/testCollectionView/testCollectionView/Alert/ViewHeaderAlert.swift
@@ -0,0 +1,111 @@
+//
+//  ViewHeaderAlert.swift
+//  testCollectionView
+//
+//  Created by Админ on 31.07.2020.
+//  Copyright © 2020 Hudihka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewHeaderAlert: UIView{
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    var dataArray = [String]()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        registrView()
+        settingsView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        registrView()
+        settingsView()
+    }
+    
+    private func registrView() {
+        
+        Bundle.main.loadNibNamed("ViewHeaderAlert", owner: self, options: nil)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(contentView)
+    }
+    
+    private func settingsView(){
+        
+        self.backgroundColor = UIColor.clear
+        
+        getData()
+        settingsCV()
+        
+        
+    }
+    
+    
+    private func getData(){
+        for i in 0...49{
+            let name = "img_\(i)"
+            dataArray.append(name)
+        }
+    }
+    
+   
+    
+    
+}
+
+
+extension ViewHeaderAlert: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
+    // MARK: - CollectionView
+
+    fileprivate func settingsCV() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        self.collectionView.backgroundColor = .clear
+
+        self.collectionView.register(UINib(nibName: "MyCustomCell", bundle: nil),
+                                     forCellWithReuseIdentifier: "MyCustomCell")
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: MyCustomCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCustomCell", for: indexPath) as! MyCustomCell
+
+
+        cell.name = dataArray[indexPath.row]
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+
+        
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = self.frame.size.width / 3.8
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+    }
+    
+    
+
+}
---
+++ алерт с кастомным хедером/testCollectionView/testCollectionView/New Group/AlertViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AlertViewController.swift
+//  testCollectionView
+//
+//  Created by Админ on 31.07.2020.
+//  Copyright © 2020 Hudihka. All rights reserved.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func playAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+
+        
+        let rect = CGRect(x: 0, y: 12, width: alertController.view.bounds.size.width, height: 102)
+        let customView = ViewHeaderAlert(frame: rect)
+
+        customView.backgroundColor = .clear
+        alertController.view.addSubview(customView)
+
+        let somethingAction = UIAlertAction(title: "Выбрать Фото", style: .default, handler: {(alert: UIAlertAction!) in print("something")})
+        somethingAction.setValue(UIColor.black, forKey: "titleTextColor")
+        
+        let deleteAction = UIAlertAction(title: "Удалить Фото", style: .default, handler: {(alert: UIAlertAction!) in print("something")})
+        deleteAction.setValue(UIColor.red, forKey: "titleTextColor")
+
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: {(alert: UIAlertAction!) in print("cancel")})
+        cancelAction.setValue(UIColor.green, forKey: "titleTextColor")
+
+        alertController.addAction(somethingAction)
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+
+        self.present(alertController, animated: true, completion:{})
+        
+    }
+    
+
+}
