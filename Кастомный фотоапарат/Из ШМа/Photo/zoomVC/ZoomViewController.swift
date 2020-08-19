7ca11b4881fadb559e4d7e4219efff490770dcdd
Константин Худышка <k.iroshnikov@itmegastar.com>
2020-08-19 13:39:30 +0700

    обновлен класс zoomVC

--- Кастомный фотоапарат/Из ШМа/Photo/zoomVC/ZoomViewController.swift
+++ Кастомный фотоапарат/Из ШМа/Photo/zoomVC/ZoomViewController.swift
@@ -172,144 +172,236 @@ extension ZoomViewController: UIScrollViewDelegate {
 
 /*
 
+ //этот код для ситуации когда изображение не входит в область дырки
 
-import Foundation
-import UIKit
+ //
+ //  ZoomViewController.swift
+ //  testCollectionView
+ //
+ //  Created by Админ on 03.08.2020.
+ //  Copyright © 2020 Hudihka. All rights reserved.
+ //
 
-class ImageScrollView: UIScrollView, UIScrollViewDelegate {
+ import Foundation
+ import UIKit
 
-    var imageZoomView: UIImageView!
-    
-    lazy var zoomingTap: UITapGestureRecognizer = {
-        let zoomingTap = UITapGestureRecognizer(target: self, action: #selector(handleZoomingTap))
-        zoomingTap.numberOfTapsRequired = 2
-        return zoomingTap
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        self.delegate = self
-        self.showsVerticalScrollIndicator = false
-        self.showsHorizontalScrollIndicator = false
-        self.decelerationRate = UIScrollViewDecelerationRateFast
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func set(image: UIImage) {
-        
-        imageZoomView?.removeFromSuperview()
-        imageZoomView = nil
-        imageZoomView = UIImageView(image: image)
-        self.addSubview(imageZoomView)
-        
-        configurateFor(imageSize: image.size)
-    }
-    
-    func configurateFor(imageSize: CGSize) {
-        self.contentSize = imageSize
-        
-        setCurrentMaxandMinZoomScale()
-        self.zoomScale = self.minimumZoomScale
-        
-        self.imageZoomView.addGestureRecognizer(self.zoomingTap)
-        self.imageZoomView.isUserInteractionEnabled = true
+ class ZoomViewController: UIViewController {
+     
+     private lazy var zoomingTap: UITapGestureRecognizer = {
+         let zoomingTap = UITapGestureRecognizer(target: self, action: #selector(handleZoomingTap))
+         zoomingTap.numberOfTapsRequired = 2
+         return zoomingTap
+     }()
+     
+     var blockNewImage: (UIImage) -> () = { _ in }
 
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        self.centerImage()
-    }
-    
-    func setCurrentMaxandMinZoomScale() {
-        let boundsSize = self.bounds.size
-        let imageSize = imageZoomView.bounds.size
-        
-        let xScale = boundsSize.width / imageSize.width
-        let yScale = boundsSize.height / imageSize.height
-        let minScale = min(xScale, yScale)
-        
-        var maxScale: CGFloat = 1.0
-        if minScale < 0.1 {
-            maxScale = 0.3
-        }
-        if minScale >= 0.1 && minScale < 0.5 {
-            maxScale = 0.7
-        }
-        if minScale >= 0.5 {
-            maxScale = max(1.0, minScale)
-        }
-        
-        self.minimumZoomScale = minScale
-        self.maximumZoomScale = maxScale
-    }
-    
-    func centerImage() {
-        let boundsSize = self.bounds.size
-        var frameToCenter = imageZoomView.frame
-        
-        if frameToCenter.size.width < boundsSize.width {
-            frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2
-        } else {
-            frameToCenter.origin.x = 0
-        }
-        
-        if frameToCenter.size.height < boundsSize.height {
-            frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2
-        } else {
-            frameToCenter.origin.y = 0
-        }
-        
-        imageZoomView.frame = frameToCenter
-    }
-    
-    // gesture
-    @objc func handleZoomingTap(sender: UITapGestureRecognizer) {
-        let location = sender.location(in: sender.view)
-        self.zoom(point: location, animated: true)
-    }
-    
-    func zoom(point: CGPoint, animated: Bool) {
-        let currectScale = self.zoomScale
-        let minScale = self.minimumZoomScale
-        let maxScale = self.maximumZoomScale
-        
-        if (minScale == maxScale && minScale > 1) {
-            return
-        }
-        
-        let toScale = maxScale
-        let finalScale = (currectScale == minScale) ? toScale : minScale
-        let zoomRect = self.zoomRect(scale: finalScale, center: point)
-        self.zoom(to: zoomRect, animated: animated)
-    }
-    
-    func zoomRect(scale: CGFloat, center: CGPoint) -> CGRect {
-        var zoomRect = CGRect.zero
-        let bounds = self.bounds
-        
-        zoomRect.size.width = bounds.size.width / scale
-        zoomRect.size.height = bounds.size.height / scale
-        
-        zoomRect.origin.x = center.x - (zoomRect.size.width / 2)
-        zoomRect.origin.y = center.y - (zoomRect.size.height / 2)
-        return zoomRect
-    }
-    
-    // MARK: - UIScrollViewDelegate
-    
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return self.imageZoomView
-    }
-    
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        self.centerImage()
-    }
-    
-}
+     @IBOutlet weak var downConstreint: NSLayoutConstraint!
+     @IBOutlet weak var scrollView: UIScrollView!
+     @IBOutlet weak var imageView: UIImageView!
+     private var imageZoom: UIImage?
+     
+     @IBOutlet weak var spiner: UIActivityIndicatorView!
+     @IBOutlet weak var saveButton: GreenButton!
+     
+     @IBOutlet weak var cupView: UIImageView!
+     
+     var hHoll: CGFloat {
+         return wDevice - 32
+     }
+     
+     var minKoef: CGFloat = 1
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         
+         downConstreint.constant = isIPhoneXorXmax ? 0 : 20
+
+         if let imageZoom = imageZoom {
+             imageView.image = imageZoom
+         }
+         
+         let zoomingTap = UITapGestureRecognizer(target: self, action: #selector(handleZoomingTap))
+         zoomingTap.numberOfTapsRequired = 2
+         self.view.addGestureRecognizer(zoomingTap)
+         
+
+         addButtons()
+
+         scrollView.delegate = self
+     }
+     
+     override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+         
+         if hImageForWindows >= hHoll {
+             scrollView.maximumZoomScale = 4
+             scrollView.minimumZoomScale = 1
+         }
+     }
+     
+     
+     override func viewDidAppear(_ animated: Bool) {
+         super.viewDidAppear(animated)
+         
+         if hImageForWindows < hHoll {
+             
+             self.minKoef = hHoll / hImageForWindows
+             let maxKoef = minKoef < 4 ? 4 : minKoef
+             scrollView.zoomScale = minKoef
+             scrollView.maximumZoomScale = maxKoef
+             scrollView.minimumZoomScale = minKoef
+             //
+             let centr = CGPoint(x: wDevice / 2, y: (hDevice / 2))
+             zoom(point: centr, animated: true)
+         }
+     }
+
+     
+     static func route(image: UIImage) -> ZoomViewController{
+         let VC = EnumStoryboard.photo.vc("ZoomViewController") as! ZoomViewController
+         VC.imageZoom = image
+         return VC
+     }
+     
+     
+     private func settingsScrollZoom(){
+         
+         if hImageForWindows < hHoll {
+             
+             self.minKoef = hHoll / hImageForWindows
+             let maxKoef = minKoef < 4 ? 4 : minKoef
+             scrollView.zoomScale = minKoef
+             scrollView.maximumZoomScale = maxKoef
+             scrollView.minimumZoomScale = minKoef
+ //
+             let centr = CGPoint(x: wDevice / 2, y: (hDevice / 2) + 100)
+             zoom(point: centr, animated: false)
+         } else {
+             scrollView.maximumZoomScale = 4
+             scrollView.minimumZoomScale = 1
+         }
+     
+     }
+     
+     private var hImageForWindows: CGFloat { //высота изображения в единицах с учетом imageView
+         
+         guard let imageZoom = imageZoom else {
+             return hHoll
+         }
+         
+         let hImageForWindows = imageZoom.size.height * hHoll / imageZoom.size.width
+         
+         return hImageForWindows
+         
+     }
+     
+
+     private func addButtons(){
+         
+         let yGrey: CGFloat = isIPhoneXorXmax ? 52 : 30
+         let xGrey: CGFloat = wDevice - 42
+         let greyRect = CGRect(x: xGrey,
+                               y: yGrey,
+                                width: 28,
+                                height: 28)
+         let crossButton = UIButton(frame: greyRect)
+         crossButton.setImage(UIImage(named: "exitBlackBold"), for: .normal)
+         crossButton.addTarget(self, action: #selector(dismiss(sender:)), for: .touchUpInside)
+         
+         self.view.addSubview(crossButton)
+         
+     }
+     
+     @IBAction func saveButton(_ sender: Any) {
+         
+         spiner.startAnimating()
+         saveButton.isEnabled = false
+         scrollView.isScrollEnabled = false
+         
+         let imageSV = scrollView.screenshot
+         cupView.image = imageSV
+         let newAvatar = cupView.screenshot
+
+         blockNewImage(newAvatar)
+
+         spiner.stopAnimating()
+         
+         self.dismiss(animated: true, completion: nil)
+     }
+     
+
+     
+     @objc private func dismiss(sender: Any) {
+         presentAddCardAlert(title: "Вы действительно что хотите выйти?", text: "Изменения не будут сохранены", textYesAction: "Выйти") { (_) in
+             self.dismiss(animated: true, completion: nil)
+         }
+     }
+     
+     
+     //MARK: gesture
+     @objc func handleZoomingTap(sender: UITapGestureRecognizer) {
+         let location = sender.location(in: sender.view)
+         self.zoom(point: location, animated: true)
+     }
+     
+     private func zoom(point: CGPoint, animated: Bool) {
+         let currectScale = self.scrollView.zoomScale
+         let minScale = self.scrollView.minimumZoomScale
+         let maxScale = self.scrollView.maximumZoomScale
+         
+         if (minScale == maxScale && minScale > minKoef) {
+             return
+         }
+         
+         let toScale = maxScale
+         let finalScale = (currectScale == minScale) ? toScale : minScale
+         let zoomRect = self.zoomRect(scale: finalScale, center: point)
+         self.scrollView.zoom(to: zoomRect, animated: animated)
+     }
+     
+     private func zoomRect(scale: CGFloat, center: CGPoint) -> CGRect {
+         var zoomRect = CGRect.zero
+         let bounds = self.scrollView.bounds
+         
+         zoomRect.size.width = bounds.size.width / scale
+         zoomRect.size.height = bounds.size.height / scale
+         
+         zoomRect.origin.x = center.x - (zoomRect.size.width / 2)
+         zoomRect.origin.y = center.y - (zoomRect.size.height / 2)
+         return zoomRect
+     }
+     
+ }
+
+
+ extension ZoomViewController: UIScrollViewDelegate {
+     
+     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+         return imageView
+     }
+     
+     func scrollViewDidZoom(_ scrollView: UIScrollView) {
+         if scrollView.zoomScale > 1 {
+             if let image = imageView.image {
+                 let ratioW = imageView.frame.width / image.size.width
+                 let ratioH = imageView.frame.height / image.size.height
+                 
+                 let ratio = ratioW < ratioH ? ratioW : ratioH
+                 let newWidth = image.size.width * ratio
+                 let newHeight = image.size.height * ratio
+                 let conditionLeft = newWidth*scrollView.zoomScale > imageView.frame.width
+                 let left = 0.5 * (conditionLeft ? newWidth - imageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+                 let conditioTop = newHeight*scrollView.zoomScale > imageView.frame.height
+                 
+                 let top = 0.5 * (conditioTop ? newHeight - imageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+                 
+                 scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+                 
+             }
+         } else {
+             scrollView.contentInset = .zero
+         }
+     }
+ }
+
 */
