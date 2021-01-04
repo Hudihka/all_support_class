a11b5e7ac901b2fd6c5394f9eb36536d1d5cbcab
Hudihka <k.iroshnikov@itmegastar.com>
2021-01-04 19:50:12 +0700

    добавлен стартовый проект

---
+++ ML Kit/SceneDetector_Starter/SceneDetector/ViewController.swift
@@ -0,0 +1,52 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  // MARK: - IBOutlets
+  @IBOutlet weak var scene: UIImageView!
+  @IBOutlet weak var answerLabel: UILabel!
+
+  // MARK: - Properties
+  let vowels: [Character] = ["a", "e", "i", "o", "u"]
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    guard let image = UIImage(named: "train_night") else {
+      fatalError("no starting image")
+    }
+
+    scene.image = image
+  }
+}
+
+// MARK: - IBActions
+extension ViewController {
+
+  @IBAction func pickImage(_ sender: Any) {
+    let pickerController = UIImagePickerController()
+    pickerController.delegate = self
+    pickerController.sourceType = .savedPhotosAlbum
+    present(pickerController, animated: true)
+  }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    dismiss(animated: true)
+
+    guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+      fatalError("couldn't load image from Photos")
+    }
+
+    scene.image = image
+  }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {
+}
