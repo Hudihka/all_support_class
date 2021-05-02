22bb3389a5da1e04092c4dcca524f72a717647ac
Hudihka <k.iroshnikov@itmegastar.com>
2021-05-02 19:15:15 +0700

    добавил баз классы

--- DI/BitcoinAdventurer/BitcoinAdventurer-Starter/Bitcoin Adventurer/ViewControllers/BitcoinViewController.swift
+++ DI/BitcoinAdventurer/BitcoinAdventurer-Starter/Bitcoin Adventurer/ViewControllers/BitcoinViewController.swift
@@ -1,30 +1,4 @@
-/// Copyright (c) 2018 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import UIKit
 
@@ -33,6 +7,8 @@ internal class BitcoinViewController: UIViewController {
   @IBOutlet weak private var checkAgain: UIButton!
   @IBOutlet weak private var primary: UILabel!
   @IBOutlet weak private var partial: UILabel!
+	
+	let networking = HTTPNetworking()
   
   private let dollarsDisplayFormatter: NumberFormatter = {
     let formatter = NumberFormatter()
@@ -66,38 +42,29 @@ internal class BitcoinViewController: UIViewController {
     partial.text = ".\(cents)"
   }
   
-  private func requestPrice()  {
-    let bitcoin = Coinbase.bitcoin.path
-    
-    // 1. Make URL request
-    guard let url = URL(string: bitcoin) else { return }
-    var request = URLRequest(url: url)
-    request.cachePolicy = .reloadIgnoringCacheData
-    
-    // 2. Make networking request
-    let task = URLSession.shared.dataTask(with: request) { data, _, error in
-      
-      // 3. Check for errors
-      if let error = error {
-        print("Error received requesting Bitcoin price: \(error.localizedDescription)")
-        return
-      }
-      
-      // 4. Parse the returned information
-      let decoder = JSONDecoder()
+	private func requestPrice()  {
+		
+		networking.request(from: Coinbase.bitcoin) { data, error in
+			// 1. Check for errors
+			if let error = error {
+				print("Error received requesting Bitcoin price: \(error.localizedDescription)")
+				return
+			}
 
-      guard let data = data,
-            let response = try? decoder.decode(PriceResponse.self,
-                                               from: data) else { return }
-      
-      print("Price returned: \(response.data.amount)")
-      
-      // 5. Update the UI with the parsed PriceResponse
-      DispatchQueue.main.async { [weak self] in
-        self?.updateLabel(price: response.data)
-      }
-    }
+			// 2. Parse the returned information
+			let decoder = JSONDecoder()
+			guard let data = data,
+						let response = try? decoder.decode(PriceResponse.self, from: data)
+			else { return }
 
-    task.resume()
-  }
+			print("Price returned: \(response.data.amount)")
+
+			// 3. Update the UI with the parsed PriceResponse
+			DispatchQueue.main.async { [weak self] in
+				self?.updateLabel(price: response.data)
+			}
+		}
+
+
+	}
 }
