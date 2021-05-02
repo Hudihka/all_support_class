9db422643d03cd1e1fa3d4c4e623e54953a93c3f
Hudihka <k.iroshnikov@itmegastar.com>
2021-05-02 22:45:16 +0700

    создал проект

---
+++ MVVM/MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Hudihka on 02/05/2021.
+//  Copyright © 2021 OOO MegaStar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
