4b87568e4f19bd91d4f62c952890ca71529d5a37
Худышка К <skinny.developer@gmail.com>
2023-03-07 21:46:45 +0400

    поиск не влезших вопросов

--- georgiaParser/georgiaParser/AppDelegate.swift
+++ georgiaParser/georgiaParser/AppDelegate.swift
@@ -45,85 +45,90 @@ struct Epic {
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //        t-num
-        let epicks = [
-            "Водитель, пассажир и пешеход, знаки, условность",
-            "Непристойность и условия вождения",
-            "Предупреждающие знаки",
-            "Знаки приоритета",
-            "Запрещающие знаки",
-            "указательные знаки",
-            "Информационно-указательные знаки",
-            "знаки обслуживания",
-            "Дополнительные информационные знаки",
-            "сигналы светофора",
-            "Сигналы регулятора",
-            "Использование специального сигнала",
-            "сигнализация аварийным маяком",
-            "Световые инструменты, звуковой сигнал",
-            "Движение, маневрирование, ходовая часть",
-            "Обгон путем обхода встречного транспортного средства",
-            "скорость движения",
-            "Тормозной путь, расстояние",
-            "перестань стоять",
-            "Проезжая перекресток",
-            "Железнодорожный переезд",
-            "Трафик на шоссе",
-            "Жилая зона, приоритет для автобуса",
-            "Буксировка",
-            "Учебный ход",
-            "Доставка, люди, груз",
-            "Велосипеды, мопеды и погони для скота",
-            "Дорожная разметка",
-            "Медицинская помощь",
-            "дорожная безопасность",
-            "Административное право",
-            "Эко-менеджмент[новый]",
-        ]
+//        let epicks = [
+//            "Водитель, пассажир и пешеход, знаки, условность",
+//            "Непристойность и условия вождения",
+//            "Предупреждающие знаки",
+//            "Знаки приоритета",
+//            "Запрещающие знаки",
+//            "указательные знаки",
+//            "Информационно-указательные знаки",
+//            "знаки обслуживания",
+//            "Дополнительные информационные знаки",
+//            "сигналы светофора",
+//            "Сигналы регулятора",
+//            "Использование специального сигнала",
+//            "сигнализация аварийным маяком",
+//            "Световые инструменты, звуковой сигнал",
+//            "Движение, маневрирование, ходовая часть",
+//            "Обгон путем обхода встречного транспортного средства",
+//            "скорость движения",
+//            "Тормозной путь, расстояние",
+//            "перестань стоять",
+//            "Проезжая перекресток",
+//            "Железнодорожный переезд",
+//            "Трафик на шоссе",
+//            "Жилая зона, приоритет для автобуса",
+//            "Буксировка",
+//            "Учебный ход",
+//            "Доставка, люди, груз",
+//            "Велосипеды, мопеды и погони для скота",
+//            "Дорожная разметка",
+//            "Медицинская помощь",
+//            "дорожная безопасность",
+//            "Административное право",
+//            "Эко-менеджмент[новый]",
+//        ]
+//
+//        var epicsArray = [Epic]()
+//
+//        for (num, obj) in epicks.enumerated() {
+//            let numbPage = num + 1
+//            var strUrls: [URL] = []
+//
+//            for pag in Array(1...20) {
+//                let str = "https://teoria.on.ge/tickets/2/\(numbPage)?page=\(pag)"
+//                if let url = URL(string: str) {
+//                    strUrls.append(url)
+//                }
+//            }
+//
+//            epicsArray.append(Epic(title: obj, urls: strUrls))
+//        }
+//
+//        var newEpics = [EpicAndNumbers]()
+//
+//
+//        for obj in epicsArray {
+//
+//            var idTask: [Int] = []
+//
+//            for objUrl in obj.urls {
+//                do {
+//                    let contents = try String(contentsOf: objUrl)
+//                    let arrayHTML = contents.components(separatedBy: "big-answers\">")
+//                    let arrayNumbers = arrayHTML
+//                        .compactMap({ $0.between("<div class=\"t-num\">#", "</div>") })
+//                        .compactMap({ Int($0) })
+//
+//                    idTask += arrayNumbers
+//                    //
+//                    print(arrayNumbers)
+//                } catch {
+//                    // contents could not be loaded
+//                }
+//            }
+//
+//            newEpics.append(EpicAndNumbers(title: obj.title, idTask: idTask))
+//
+//        }
         
-        var epicsArray = [Epic]()
-        
-        for (num, obj) in epicks.enumerated() {
-            let numbPage = num + 1
-            var strUrls: [URL] = []
-            
-            for pag in Array(1...20) {
-                let str = "https://teoria.on.ge/tickets/2/\(numbPage)?page=\(pag)"
-                if let url = URL(string: str) {
-                    strUrls.append(url)
-                }
-            }
-            
-            epicsArray.append(Epic(title: obj, urls: strUrls))
+        var count = 0
+        Content.shared.newEpics.forEach { str in
+            count += str.idTask.count
         }
         
-        var newEpics = [EpicAndNumbers]()
-    
-        
-        for obj in epicsArray {
-            
-            var idTask: [Int] = []
-            
-            for objUrl in obj.urls {
-                do {
-                    let contents = try String(contentsOf: objUrl)
-                    let arrayHTML = contents.components(separatedBy: "big-answers\">")
-                    let arrayNumbers = arrayHTML
-                        .compactMap({ $0.between("<div class=\"t-num\">#", "</div>") })
-                        .compactMap({ Int($0) })
-                    
-                    idTask += arrayNumbers
-                    //
-                    print(arrayNumbers)
-                } catch {
-                    // contents could not be loaded
-                }
-            }
-            
-            newEpics.append(EpicAndNumbers(title: obj.title, idTask: idTask))
-            
-        }
-        
-        print(newEpics)
+        print(count)
         
         
         // http://teoria.on.ge/tickets/2/1
