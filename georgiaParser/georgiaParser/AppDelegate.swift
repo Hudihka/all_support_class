0fd3e1f84e010fa24afe4738d0be55d003948e52
Худышка К <skinny.developer@gmail.com>
2023-03-07 22:00:11 +0400

    перехожу к созданию вопросов

--- georgiaParser/georgiaParser/AppDelegate.swift
+++ georgiaParser/georgiaParser/AppDelegate.swift
@@ -45,89 +45,91 @@ struct Epic {
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //        t-num
-//        let epicks = [
-//            "Водитель, пассажир и пешеход, знаки, условность",
-//            "Непристойность и условия вождения",
-//            "Предупреждающие знаки",
-//            "Знаки приоритета",
-//            "Запрещающие знаки",
-//            "указательные знаки",
-//            "Информационно-указательные знаки",
-//            "знаки обслуживания",
-//            "Дополнительные информационные знаки",
-//            "сигналы светофора",
-//            "Сигналы регулятора",
-//            "Использование специального сигнала",
-//            "сигнализация аварийным маяком",
-//            "Световые инструменты, звуковой сигнал",
-//            "Движение, маневрирование, ходовая часть",
-//            "Обгон путем обхода встречного транспортного средства",
-//            "скорость движения",
-//            "Тормозной путь, расстояние",
-//            "перестань стоять",
-//            "Проезжая перекресток",
-//            "Железнодорожный переезд",
-//            "Трафик на шоссе",
-//            "Жилая зона, приоритет для автобуса",
-//            "Буксировка",
-//            "Учебный ход",
-//            "Доставка, люди, груз",
-//            "Велосипеды, мопеды и погони для скота",
-//            "Дорожная разметка",
-//            "Медицинская помощь",
-//            "дорожная безопасность",
-//            "Административное право",
-//            "Эко-менеджмент[новый]",
-//        ]
+        let epicks = [
+            "Водитель, пассажир и пешеход, знаки, условность",
+            "Непристойность и условия вождения",
+            "Предупреждающие знаки",
+            "Знаки приоритета",
+            "Запрещающие знаки",
+            "указательные знаки",
+            "Информационно-указательные знаки",
+            "знаки обслуживания",
+            "Дополнительные информационные знаки",
+            "сигналы светофора",
+            "Сигналы регулятора",
+            "Использование специального сигнала",
+            "сигнализация аварийным маяком",
+            "Световые инструменты, звуковой сигнал",
+            "Движение, маневрирование, ходовая часть",
+            "Обгон путем обхода встречного транспортного средства",
+            "скорость движения",
+            "Тормозной путь, расстояние",
+            "перестань стоять",
+            "Проезжая перекресток",
+            "Железнодорожный переезд",
+            "Трафик на шоссе",
+            "Жилая зона, приоритет для автобуса",
+            "Буксировка",
+            "Учебный ход",
+            "Доставка, люди, груз",
+            "Велосипеды, мопеды и погони для скота",
+            "Дорожная разметка",
+            "Медицинская помощь",
+            "дорожная безопасность",
+            "Административное право",
+            "Эко-менеджмент[новый]",
+        ]
+
+        var epicsArray = [Epic]()
+
+        for (num, obj) in epicks.enumerated() {
+            let numbPage = num + 1
+            var strUrls: [URL] = []
+
+            for pag in Array(1...20) {
+                let str = "https://teoria.on.ge/tickets/2/\(numbPage)?page=\(pag)"
+                if let url = URL(string: str) {
+                    strUrls.append(url)
+                }
+            }
+
+            epicsArray.append(Epic(title: obj, urls: strUrls))
+        }
+
+        var newEpics = [EpicAndNumbers]()
+
+
+        for obj in epicsArray {
 //
-//        var epicsArray = [Epic]()
+            var idTask: [Int] = []
 //
-//        for (num, obj) in epicks.enumerated() {
-//            let numbPage = num + 1
-//            var strUrls: [URL] = []
-//
-//            for pag in Array(1...20) {
-//                let str = "https://teoria.on.ge/tickets/2/\(numbPage)?page=\(pag)"
-//                if let url = URL(string: str) {
-//                    strUrls.append(url)
-//                }
-//            }
-//
-//            epicsArray.append(Epic(title: obj, urls: strUrls))
-//        }
-//
-//        var newEpics = [EpicAndNumbers]()
-//
-//
-//        for obj in epicsArray {
-//
-//            var idTask: [Int] = []
-//
-//            for objUrl in obj.urls {
-//                do {
-//                    let contents = try String(contentsOf: objUrl)
-//                    let arrayHTML = contents.components(separatedBy: "big-answers\">")
-//                    let arrayNumbers = arrayHTML
-//                        .compactMap({ $0.between("<div class=\"t-num\">#", "</div>") })
-//                        .compactMap({ Int($0) })
-//
-//                    idTask += arrayNumbers
-//                    //
-//                    print(arrayNumbers)
-//                } catch {
-//                    // contents could not be loaded
-//                }
-//            }
-//
-//            newEpics.append(EpicAndNumbers(title: obj.title, idTask: idTask))
-//
-//        }
-        
-        var count = 0
-        Content.shared.newEpics.forEach { str in
-            count += str.idTask.count
+            for objUrl in obj.urls {
+                do {
+                    let contents = try String(contentsOf: objUrl)
+                    let arrayHTML = contents.components(separatedBy: "<div class=\"t-num\">")
+                    let arrayNumbers = arrayHTML
+                        .compactMap({ $0.between("#", "</div>") })
+                        .compactMap({ Int($0) })
+
+                    idTask += arrayNumbers
+                    //
+                    print(arrayNumbers)
+                } catch {
+                    // contents could not be loaded
+                }
+        print(idTask)
+            }
+
+            newEpics.append(EpicAndNumbers(title: obj.title, idTask: idTask))
+
         }
         
+        var count = 0
+        newEpics.forEach { str in
+            count += str.idTask.count
+        }
+//
+        print(newEpics)
         print(count)
         
         
