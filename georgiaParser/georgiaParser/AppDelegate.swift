985c54d547c12f3bd445b39cc85b7f7c904aea42
Худышка К <skinny.developer@gmail.com>
2023-05-06 22:22:00 +0400

    создал эпики

--- georgiaParser/georgiaParser/AppDelegate.swift
+++ georgiaParser/georgiaParser/AppDelegate.swift
@@ -14,9 +14,84 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private let content = Content.shared
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //        1782
+        //        1782  не верный 2991
         
-        var setsEpic: Set<String> = []
+//        <title>ეკო-მართვა ბილეთი #1782
+//        <title>მოძრაობის უსაფრთხოება ბილეთი #839
+        
+//        print(getTextHTML("https://teoria.on.ge/tickets?ticket=839"))
+        
+        let georgian = [
+            "ველოსიპედი, მოპედი და პირუტყვის გადარეკვა",
+            "სამუხრუჭე მანძილი, დისტანცია",
+            "მოძრაობის უსაფრთხოება",
+            "სამედიცინო დახმარება",
+            "გაჩერება დგომა",
+            "პრიორიტეტის ნიშნები",
+            "მძღოლი, მგზავრი და ქვეითი, ნიშნები, კონვეცია",
+            "მაფრთხილებელი ნიშნები",
+            "გზაჯვარედინის გავლა",
+            "ეკო-მართვა",
+            "რკინიგზის გადასასვლელი",
+            "დამატებითი ინფორმაციის ნიშნები",
+            "საგზაო მონიშვნა",
+            "სანათი ხელსაწყოები, ხმოვანი სიგნალი",
+            "მოძრაობის სიჩქარე",
+            "სასწავლო სვლა",
+            "მიმთითებელი ნიშნები",
+            "სპეციალური სიგნალის გამოყენება",
+            "საავარიო შუქური სიგნალიზაცია",
+            "ამკრძალავი ნიშნები",
+            "გადაზიდვები, ხალხი, ტვირთი",
+            "ბუქსირება",
+            "მოძრაობა, მანევრირება, სავალი ნაწილი",
+            "გასწრება შემხვედრის გვერდის ავლით",
+            "მოძრაობა ავტომაგისტრალზე",
+             "მარეგულირებლის სიგნალები",
+            "შუქნიშნის სიგნალები",
+            "საინფორმაციო-მაჩვენებელი ნიშნები",
+            "საცხოვრებელი ზონა, სამარშრუტოს პრიორიტეტი",
+             "სერვისის ნიშნები",
+            "უწესივრობა და მართვის პირობები",
+            "ადმინისტრაციული კანონი"
+        ]
+        
+        let russian = [
+            "Велосипед, мопед и вождение скота",
+            "тормозной путь, дистанция",
+            "Дорожная безопасность",
+            "Медицинская помощь",
+            "перестань стоять",
+            "Знаки приоритета",
+            "Водитель, пассажир и пешеход, знаки, условность",
+            "предупреждающие знаки",
+            "Перекресток",
+            "Эко-менеджмент",
+            "Железнодорожный переезд",
+            "Дополнительные информационные знаки",
+            "дорожная разметка",
+            "Светотехника, звуковой сигнал",
+            "скорость движения",
+            "обучающий ход",
+            "указательные знаки",
+            "По специальному сигналу",
+            "Сигнализация аварийного маяка",
+            "запрещающие знаки",
+            "Доставка, Люди, Груз",
+            "буксировка",
+            "движение, маневрирование, ходовая часть",
+            "обгон путем обхода встречного транспортного средства",
+            "Движение по шоссе",
+            "Сигналы регулятора",
+            "Сигналы светофора",
+            "Информационно-указательные знаки",
+            "Жилая зона, автобусный приоритет",
+            "Знаки обслуживания",
+            "Неровности и условия движения",
+            "Административное право"
+        ]
+        
+        var setsEpic: [String] = []
         var arrayNumberQwestion: [Int] = []
 
         // название темы
@@ -25,26 +100,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // фаза 1 формирование тем и вопросов
         
-//        DispatchQueue.global().async {
-//            for i in 0...3000 {
-//                let url = "https://teoria.on.ge/tickets?ticket=\(i)"
-//                print(i)
-//                if
-//                    let text = self.getTextHTML(url),
-//                    text.contains("[B, B1]"),
-//                    let title = text.between("<a  href=\"/tickets/0/32\" title=\"", "\"><span class=\"id\">")
-//                {
-//                    arrayNumberQwestion.append(i)
-//                    setsEpic.insert(title)
-//                }
-//            }
-//            
-//            print(arrayNumberQwestion.count)
-//            print(setsEpic.count)
-//            print(setsEpic)
-//        
-//            
-//        }
+        DispatchQueue.global().async {
+            for i in 0...3000 {
+                print(i)
+                let url = "https://teoria.on.ge/tickets?ticket=\(i)"
+                if
+                    let text = self.getTextHTML(url),
+                    text.contains("[B, B1]"),
+                    let title = text.between("<title>", " ბილეთი #\(i)"),
+                    let geoggianIndex = georgian.firstIndex(where: { $0 == title }),
+                    let russianText = russian[safe: geoggianIndex]?.capitalizingFirstLetter()
+                {
+                    if let indexRussianStruct = self.content.epicks.firstIndex(where: { $0.russName == russianText}) {
+                        let old = self.content.epicks[indexRussianStruct]
+                        let newArray = old.arrayNumbers + [i]
+                        let new = Epic(
+                            number: old.number,
+                            russName: russianText,
+                            arrayNumbers: newArray
+                        )
+                        self.content.epicks[indexRussianStruct] = new
+                    } else {
+                        let index = self.content.epicks.count
+                        let new = Epic(
+                            number: index,
+                            russName: russianText,
+                            arrayNumbers: [i]
+                        )
+                        self.content.epicks.append(new)
+                    }
+                }
+            }
+
+            print(self.content.epicks)
+
+
+        }
+        
+        
         
         // фаза 2 формирование дикшинари [тема : список вопросов]
         
@@ -66,17 +159,33 @@ private extension AppDelegate {
         }
     }
     
-    func epicQwestions(georgianEpic: String, number: Int) {
-        guard let keyRussian = content.epicks[georgianEpic] else {
-            return
-        }
-        
-        if var arrayRussian = content.epicQwestions[keyRussian], arrayRussian.isEmpty == false {
-                arrayRussian.append(number)
-                content.epicQwestions[keyRussian] = arrayRussian
-            
-        } else {
-            content.epicQwestions[keyRussian] = [number]
-        }
+//    func epicQwestions(georgianEpic: String, number: Int) {
+//        guard let keyRussian = content.epicks[georgianEpic] else {
+//            return
+//        }
+//
+//        if var arrayRussian = content.epicQwestions[keyRussian], arrayRussian.isEmpty == false {
+//                arrayRussian.append(number)
+//                content.epicQwestions[keyRussian] = arrayRussian
+//
+//        } else {
+//            content.epicQwestions[keyRussian] = [number]
+//        }
+//    }
+}
+
+extension Array {
+    subscript (safe index: Index) -> Element? {
+        0 <= index && index < count ? self[index] : nil
+    }
+}
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
     }
 }
