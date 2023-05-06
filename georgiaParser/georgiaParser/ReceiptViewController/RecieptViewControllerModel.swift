02c40ffc411ab893496953d6ba6493d2b89e9484
Худышка К <skinny.developer@gmail.com>
2023-05-07 02:25:07 +0400

    сделал парсер, начинаю проверку

--- georgiaParser/georgiaParser/ReceiptViewController/RecieptViewControllerModel.swift
+++ georgiaParser/georgiaParser/ReceiptViewController/RecieptViewControllerModel.swift
@@ -22,15 +22,12 @@ final class RecieptViewControllerModel: RecieptViewControllerProtocolIn,
 
     func fetchData() {
         guard
-            let firstNumber = contentEpics.first?.arrayNumbers.first,
-            let url = generateUrlWith(firstNumber)
+            let firstNumber = contentEpics.first?.arrayNumbers.first
         else {
             return
         }
         
-        self.number = firstNumber
-        
-        content(url)
+        updateConent(firstNumber)
     }
     
     func next(html: Any?) {
@@ -42,9 +39,54 @@ final class RecieptViewControllerModel: RecieptViewControllerProtocolIn,
             fatalError()
         }
         
-        
-        
         localQwestion.append(qwestion)
+        
+        if
+            let firstEpic = contentEpics.first(where: { $0.arrayNumbers.contains(number) }),
+            let indexEpic = contentEpics.firstIndex(where: { $0.russName == firstEpic.russName }),
+            let lastNumber = firstEpic.arrayNumbers.last
+        {
+            if lastNumber == number {
+                
+                let newAnswers = EpicWithQwestion(name: firstEpic.russName, qwestions: localQwestion)
+                localQwestionAnswers.append(newAnswers)
+                localQwestion = []
+                
+                
+                if let nextepic = contentEpics[safe: indexEpic + 1], let newFirst = nextepic.arrayNumbers.first {
+                    // новый эпик
+                    updateConent(newFirst)
+                } else {
+                    // закончились эпики
+                    contentSingl.epicQwestions = localQwestionAnswers
+                    print("-----------------------------------------------------------")
+                    print("-----------------------------------------------------------")
+                    print("-----------------------------------------------------------")
+                    print(contentSingl.epicQwestions)
+                    print("-----------------------------------------------------------")
+                    print("-----------------------------------------------------------")
+                    print("-----------------------------------------------------------")
+                    
+                    print("всего \(contentSingl.epicQwestions.count) темы")
+                    
+                    var summ = 0
+                    for i in contentSingl.epicQwestions.map({ $0.qwestions.count }) {
+                        summ += i
+                    }
+                    
+                    print("всего \(summ) вопроса")
+                    
+                    print("-----------------------------------------------------------")
+                    print("-----------------------------------------------------------")
+                    print("-----------------------------------------------------------")
+                }
+            } else if
+                let indexQwestion = firstEpic.arrayNumbers.firstIndex(where: { $0 == self.number }),
+                let next = firstEpic.arrayNumbers[safe: indexQwestion + 1]
+            {
+                updateConent(next)
+            }
+        }
         
         
         
@@ -54,5 +96,17 @@ final class RecieptViewControllerModel: RecieptViewControllerProtocolIn,
 private extension RecieptViewControllerModel {
     func generateUrlWith(_ number: Int) -> URL? {
         URL(string: "https://teoria.on.ge/tickets?ticket=\(number)")
+    }
+    
+    func updateConent(_ index: Int) {
+        guard
+            let url = generateUrlWith(index)
+        else {
+            fatalError()
+        }
+        
+        self.number = index
+        
+        content(url)
     }
 }
