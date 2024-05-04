533ecdb620bf6dba66df48ac9135bf9b254bb0df
Худышка <skinny.developer@gmail.com>
2024-05-04 05:03:43 +0200

    задача с газом

--- Алгоритмы/Algoritms/Algoritms/Example/GasStation.swift
+++ Алгоритмы/Algoritms/Algoritms/Example/GasStation.swift
@@ -11,7 +11,35 @@ import Foundation
 
 final class GasStation: TesterProtocol {
     func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
-        0
+        let badResult = -1
+        
+        var casOnBag = 0
+        
+        for i in 0..<gas.count {
+            let newGas = gas.separateOn(index: i)
+            let newCost = gas.separateOn(index: i)
+            casOnBag = 0
+            
+            for (offset, value) in newGas.enumerated() {
+                if offset == 0, value == 0 {
+                    return badResult
+                }
+                
+                if casOnBag < 0 {
+                    return badResult
+                }
+                
+                casOnBag += (value - newCost[offset])
+            }
+            
+            if casOnBag >= 0 {
+                return i
+            }
+            
+        }
+        
+        
+        return badResult
     }
     
     func test() {
@@ -22,5 +50,25 @@ final class GasStation: TesterProtocol {
         print(
             canCompleteCircuit([2,3,4], [3,4,3]) == -1
         )
+    }
+}
+
+extension Array {
+    func separateOn(index: Int) -> Self {
+        guard index != 0 else {
+            return self
+        }
+        
+        var returnArray: Self = []
+        
+        for i in index..<self.count {
+            returnArray.append(self[i])
+        }
+        
+        for i in 0..<index {
+            returnArray.append(self[i])
+        }
+        
+        return returnArray
     }
 }
